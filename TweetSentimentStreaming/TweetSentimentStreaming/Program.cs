bbf9ad7b0a89ebd2fc4d98994be02049bc8933a1
unknown <guizha@WIN-B8QK646IHKC.fareast.corp.microsoft.com>
2014-10-29 17:51:15 +0800

    first commit Analyze real-time Twitter sentiment with HBase in HDInsight

---
+++ TweetSentimentStreaming/TweetSentimentStreaming/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+
+namespace TweetSentimentStreaming
+{
+    class Program
+    {
+        const string TWITTERAPPACCESSTOKEN = "2881765675-LT5sH57L8N0m2Kiai0bLdf5Oc6H0dydCgggaTrX";
+        const string TWITTERAPPACCESSTOKENSECRET = "7m83Gse9F330BSbn4rzITt08v6aCNGSL7AdYjsQ7iGn9a";
+        const string TWITTERAPPAPIKEY = "ODScGthram80EhHg6owadojR7";
+        const string TWITTERAPPAPISECRET = "YsT5RdVnBpr3FDOk9fh0eTRInR3GR1QqnYD3diHzEsnsvKP9Vo";
+
+        static void Main(string[] args)
+        {
+            TwitterCredentials.SetCredentials(TWITTERAPPACCESSTOKEN, TWITTERAPPACCESSTOKENSECRET, TWITTERAPPAPIKEY, TWITTERAPPAPISECRET);
+
+            Stream_FilteredStreamExample();
+        }
+
+        private static void Stream_FilteredStreamExample()
+        {
+            for (; ; )
+            {
+                try
+                {
+                    HBaseWriter hbase = new HBaseWriter();
+                    var stream = Stream.CreateFilteredStream();
+                    stream.AddLocation(Geo.GenerateLocation(-180, -90, 180, 90));
+
+                    var tweetCount = 0;
+                    var timer = Stopwatch.StartNew();
+
+                    stream.MatchingTweetReceived += (sender, args) =>
+                    {
+                        tweetCount++;
+                        var tweet = args.Tweet;
+
+                        // Write Tweets to HBase
+                        hbase.WriteTweet(tweet);
+
+                        if (timer.ElapsedMilliseconds > 1000)
+                        {
+                            if (tweet.Coordinates != null)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine("\n{0}: {1} {2}", tweet.Id, tweet.Language.ToString(), tweet.Text);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.WriteLine("\tLocation: {0}, {1}", tweet.Coordinates.Longitude, tweet.Coordinates.Latitude);
+                            }
+
+                            timer.Restart();
+                            Console.WriteLine("\tTweets/sec: {0}", tweetCount);
+                            tweetCount = 0;
+                        }
+                    };
+
+                    stream.StartStreamMatchingAllConditions();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception: {0}", ex.Message);
+                }
+            }
+        }
+    }
+}
